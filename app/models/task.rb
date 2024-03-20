37bec247e0bc1638cc4485f2e43090b87b144aba
kakru Aoyama <kakeru0520sou1@gmail.com>
2024-03-21 02:10:58 +0900

    グラフ表示

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  def self.done_yet_rate
+    all_count = all.count
+    done_count = where(status: "done").count
+    done_rate = done_count / all_count.to_f
+    yet_rate = 1 - done_rate
+    return [done_rate, yet_rate].map { |rate| (rate * 100).round(2) }
+  end
 end
