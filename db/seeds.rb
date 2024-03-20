dc7d1f26e4aae43c07fc4e34d4d1a5c34a03e6ab
kakru Aoyama <kakeru0520sou1@gmail.com>
2024-03-21 02:30:13 +0900

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# ユーザーデータの生成
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: 'password',
+  )
+end
+
+# タスクデータの生成
+50.times do
+  Task.create!(
+    title: Faker::Lorem.sentence(word_count: 3),
+    description: Faker::Lorem.sentence(word_count: 10),
+    due_date: Faker::Date.forward(days: 30),
+    status: ['yet', 'done'].sample,
+    user_id: User.pluck(:id).sample
+  )
+end
